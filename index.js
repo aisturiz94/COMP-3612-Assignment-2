5ccf3a8d6be96d60ff55f46754998315e805a020
SubterraGuy <josenari12@gmail.com>
2025-11-19 10:13:33 -0700

    JavaScript implementation for loading the gender filtered products

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    let pageButtons = document.querySelectorAll("button");
+    const url =  "https://gist.githubusercontent.com/rconnolly/d37a491b50203d66d043c26f33dbd798/raw/37b5b68c527ddbe824eaed12073d266d5455432a/clothing-compact.json";
 
-    loadData();
+    let pageButtons = document.querySelectorAll("nav button");
+    let products = [];
 
-    //Adds an event listener to all the buttons
-    pageButtons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        if (pageButtonClicked(e.target.id)) {
-            changePage(e.target.id);
-        }
-    });
-});
     
-    // Verifies if the data is currently stored and 
-    // If the data is not stored then it gets the data.
-    async function loadData() {
-            const savedData = localStorage.getItem("products");
+    const savedData = localStorage.getItem("products");
 
-            if (savedData) {
-                return JSON.parse(savedData);
-            }
+    if (savedData) {
+        products = JSON.parse(savedData);
 
-            const response = await fetch("https://gist.githubusercontent.com/rconnolly/...");
-            const data = await response.json();
+        setNavButtonListener(products);
+    }
 
+    else{
+        console.log("before");
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
             localStorage.setItem("products", JSON.stringify(data));
+            products = data;
 
-            return data;
+            setNavButtonListener(products);
+    });
+    }
+
+    //Adds an event listener to all the navigation buttons
+    function setNavButtonListener(products){
+        pageButtons.forEach(button => {
+            button.addEventListener("click", (e) => {
+                if (pageButtonClicked(e.target.id)) {
+                    changePage(e.target.id, products);
+                }
+            });
+        });
     }
 
 
@@ -44,9 +51,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Changes the web page being displayed by hiding
     // all of them and setting the selected page visible.
-    function changePage(viewId) {
+    function changePage(viewId, products) {
             const views = ["home-view", "women-view", "men-view" ,"browse-view", "product-view", "cart-view"];
 
+            if (viewId == "women" || viewId == "men"){
+                loadGenderProducts(viewId, products);
+            }
             views.forEach(view => {
                 document.getElementById(view).classList.add("hidden");
             });
@@ -54,5 +64,47 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById(`${viewId}-view`).classList.remove("hidden");
     }
 
+    // The function creates filtered products based on the gender that was chosen
+    function loadGenderProducts (genderView, products) {
+            let productSection = document.querySelector(`#${genderView}-products`);
+            productSection.innerHTML = '';
+
+            products.forEach(product => {
+                if (product.gender == `${genderView}s`) {
+                    let figure = document.createElement("figure");
+
+                    // ARE WE GOING TO ADD IMAGES OR NAH?
+                    let img = document.createElement("img");
+                    img.src = "";
+                    img.alt = product.name;
+
+                    let figcaption = document.createElement("figcaption");
+
+                    let productName = document.createElement("p");
+                    productName.textContent = product.name;
+
+                    let productPrice = document.createElement("p");
+                    productPrice.textContent = `$${product.price}`;
+
+                    let addBtn = document.createElement("button");
+                    addBtn.textContent = "ADD";
+                    addBtn.dataset.productId = product.id;
+
+                    let detailBtn = document.createElement("button");
+                    detailBtn.textContent = "MORE DETAILS";
+                    detailBtn.dataset.productId = product.id;
+
+                    figcaption.appendChild(productName);
+                    figcaption.appendChild(productPrice);
+                    figcaption.appendChild(addBtn);
+                    figcaption.appendChild(detailBtn);
+
+                    figure.appendChild(img);
+                    figure.appendChild(figcaption);
+
+                    productSection.appendChild(figure);
+                }
+            });
+    }
 
 });
